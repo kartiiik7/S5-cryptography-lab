f454a846512770c5f0691d36026bd423c40d0efb
kartiiik7 <kartikanil2004@gmail.com>
2025-09-01 21:26:27 +0530

    Update blowfish.java

--- S5 Cryptography/programs/blowfish.java
+++ S5 Cryptography/programs/blowfish.java
@@ -1,19 +1,28 @@
 import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec; 
-import java.util.Scanner;
-public class blowfish {
-    public static byte[] encrypt(byte[] plaintext, byte[] key) throws Exception { 
-        Cipher cipher = Cipher.getInstance("Blowfish"); cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key, "Blowfish")); return cipher.doFinal(plaintext);
-    }
-    public static byte[] decrypt(byte[] ciphertext, byte[] key) throws Exception{ 
-        Cipher cipher = Cipher.getInstance("Blowfish");
-        cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(key, "Blowfish")); 
-        return cipher.doFinal(ciphertext);
-    }
-    public static void main(String[] args) throws Exception { 
-        byte[] key = "my_secret_key".getBytes();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the plain text: "); byte[] plaintext = sc.nextLine().getBytes(); byte[] ciphertext = encrypt(plaintext, key);
-        System.out.println("Ciphertext: " + new String(ciphertext)); byte[] decrypted = decrypt(ciphertext, key); System.out.println("Decrypted: " + new String(decrypted));
-    }
+import javax.crypto.SecretKey;
+import java.crypto.KeyGenerator;
+import java.swing.JOptionPane;
+import java.util.Base64;
+
+public class Blowfish {
+public static byte[] encrypt(byte[] plaintext, byte[] key) throws Exception { Cipher cipher = Cipher.getInstance("Blowfish"); return cipher.doFinal(plaintext);
+}
+
+public static byte[] decrypt(byte[] ciphertext, byte[] key) throws Exception { 
+	return 
+}
+public static void main(String[] args) throws Exception {
+KeyGenerator keyGenerator= keyGenerator.getInstance("Blowfish");
+KeyGenerator.init(128);
+Cipher cipher = Cipher.getInstance("Blowfish");
+String inputText=JOptionPane.showInputDialog(“Input the message”);
+cipher.init(Cipher.ENCRYPT_MODE,SecretKey);
+byte[] encryptedBytes=cipher.doFinal(inputText.getBytes());
+String encryptedText=Base64.getEncoder().encodeToString(encryptedBytes);
+cipher.init(Cipher.DECRYPT_MODE,SecretKey);
+byte[] decryptedBytes= cipher.doFinal(encryptedBytes);
+JOptionPane.showMessageDialog(JOption.getRootFrame(),
+”Original Text: ”+inputText+”\n”+”Encrypted Text: ”+ new string (encryptedText)
++”\n”+” Decrypted Text: ”+”new string(decryptedBytes)”;
+}
 }
