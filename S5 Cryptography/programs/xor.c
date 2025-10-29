5bd6533797d00a9df4544824c4b581957b86eb4c
kartiiik7 <kartikanil2004@gmail.com>
2025-10-29 15:38:22 +0530

    Delete S5 Cryptography directory

--- S5 Cryptography/programs/xor.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main(){
-    char input[]="helloworld";
-    char result[sizeof(input)];
-    for(int i=0;i<sizeof(input)-1;i++)
-        result[i]=input[i]^0;
-    printf("Original String: %s\n",input);
-    printf("XORed String: %s\n",result);
-}
